e1b63b7be731f51bad7ec7f9b873840fa8e283e0
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-08-14 17:02:28 -0300

    corrige logica de exames

--- db/initialize_db.rb
+++ db/initialize_db.rb
@@ -50,8 +50,10 @@ module Database
                  id SERIAL PRIMARY KEY,
                  id_patient INTEGER REFERENCES patients (id),
                  id_doctor INTEGER REFERENCES doctors (id),
-                 token VARCHAR(14) UNIQUE NOT NULL,
-                 date DATE
+                 token VARCHAR(14) NOT NULL,
+                 type VARCHAR(150) NOT NULL,
+                 date DATE,
+                 UNIQUE (token, type, date)
                )')
   end
 
@@ -76,92 +78,74 @@ module Database
   end
 
   def self.insert_patient(row, conn)
-    patient_result = conn.exec_params(
-      'SELECT id FROM patients WHERE cpf = $1 LIMIT 1',
-      [row['cpf']]
+    conn.exec_params(
+      'INSERT INTO patients (cpf, name, email, birthday, address, city, state)
+       VALUES ($1, $2, $3, $4, $5, $6, $7)
+       ON CONFLICT (cpf) DO NOTHING',
+      [
+        row['cpf'],
+        row['nome paciente'],
+        row['email paciente'],
+        Date.parse(row['data nascimento paciente']),
+        row['endereço/rua paciente'],
+        row['cidade paciente'],
+        row['estado patiente']
+      ]
     )
-    if patient_result.num_tuples.zero?
-      conn.exec_params(
-        'INSERT INTO patients (cpf, name, email, birthday, address, city, state)
-         VALUES ($1, $2, $3, $4, $5, $6, $7)',
-        [
-          row['cpf'],
-          row['nome paciente'],
-          row['email paciente'],
-          Date.parse(row['data nascimento paciente']),
-          row['endereço/rua paciente'],
-          row['cidade paciente'],
-          row['estado patiente']
-        ]
-      )
-    end
   end
 
   def self.insert_doctor(row, conn)
-    doctor_result = conn.exec_params(
-      'SELECT id FROM doctors WHERE crm = $1 LIMIT 1',
-      [row['crm médico']]
+    conn.exec_params(
+      'INSERT INTO doctors (crm, crm_state, name, email)
+       VALUES ($1, $2, $3, $4)
+       ON CONFLICT (crm) DO NOTHING',
+      [
+        row['crm médico'],
+        row['crm médico estado'],
+        row['nome médico'],
+        row['email médico']
+      ]
     )
-    if doctor_result.num_tuples.zero?
-      conn.exec_params(
-        'INSERT INTO doctors (crm, crm_state, name, email)
-         VALUES ($1, $2, $3, $4)',
-        [
-          row['crm médico'],
-          row['crm médico estado'],
-          row['nome médico'],
-          row['email médico']
-        ]
-      )
-    end
   end
 
   def self.insert_exam(row, conn)
-    exam_result = conn.exec_params(
-      'SELECT id FROM exams WHERE token = $1 LIMIT 1',
-      [row['token resultado exame']]
+    conn.exec_params(
+      'INSERT INTO exams (id_patient, id_doctor, token, type, date)
+       VALUES (
+         (SELECT id FROM patients WHERE cpf = $1),
+         (SELECT id FROM doctors WHERE crm = $2),
+         $3,
+         $4,
+         $5
+       )
+       ON CONFLICT (token, type, date) DO NOTHING',
+      [
+        row['cpf'],
+        row['crm médico'],
+        row['token resultado exame'],
+        row['tipo exame'],
+        Date.parse(row['data exame'])
+      ]
     )
-    if exam_result.num_tuples.zero?
-      conn.exec_params(
-        'INSERT INTO exams (id_patient, id_doctor, token, date)
-         VALUES (
-           (SELECT id FROM patients WHERE cpf = $1),
-           (SELECT id FROM doctors WHERE crm = $2),
-           $3,
-           $4
-         )',
-        [
-          row['cpf'],
-          row['crm médico'],
-          row['token resultado exame'],
-          Date.parse(row['data exame'])
-        ]
-      )
-    end
   end
 
   def self.insert_exam_result(row, conn)
     id_exam_result = conn.exec_params(
-      'SELECT id FROM exams WHERE token = $1 LIMIT 1',
-      [row['token resultado exame']]
+      'SELECT id FROM exams WHERE token = $1 AND type = $2 AND date = $3 LIMIT 1',
+      [row['token resultado exame'], row['tipo exame'], Date.parse(row['data exame'])]
     )[0]['id'].to_i
 
-    result_exists = conn.exec_params(
-      'SELECT * FROM exam_results WHERE id_exam = $1 AND type = $2 LIMIT 1',
-      [id_exam_result, row['tipo exame']]
-    ).num_tuples.zero?
-    if result_exists
-      conn.exec_params(
-        'INSERT INTO exam_results (id_exam, type, limits, result)
-         VALUES ($1, $2, $3, $4)',
-        [
-          id_exam_result,
-          row['tipo exame'],
-          row['limites tipo exame'],
-          row['resultado tipo exame']
-        ]
-      )
-    end
+    conn.exec_params(
+      'INSERT INTO exam_results (id_exam, type, limits, result)
+       VALUES ($1, $2, $3, $4)
+       ON CONFLICT (id_exam, type) DO NOTHING',
+      [
+        id_exam_result,
+        row['tipo exame'],
+        row['limites tipo exame'],
+        row['resultado tipo exame']
+      ]
+    )
   end
 
   def self.read_from_csv(file)
